f048cd7b53bc6333621065c010aaa24f5a5bd1db
amrohan <rohanxsalunkhe@gmail.com>
2025-12-20 19:27:11 +0530

    feat: add calendar

---
+++ src/app/pages/tracker/tracker-card/tracker-card.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tracker-card',
+  imports: [],
+  template: `
+    <div class="card card-xs md:card-sm bg-base-200 rounded-lg">
+      <div class="card-body">
+        <div class="flex justify-between items-center gap-4">
+          <div>
+            <h2 class="card-title">
+              Gulugulu Day
+            </h2>
+            <p class="text-base-content/80">22 Aug 2025</p>
+          </div>
+          <button class="btn btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 class="size-[1.2em]">
+              <circle cx="12" cy="12" r="1"/>
+              <circle cx="12" cy="5" r="1"/>
+              <circle cx="12" cy="19" r="1"/>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: ``
+})
+export class TrackerCard {
+
+}
--- src/app/pages/tracker/tracker.ts
+++ src/app/pages/tracker/tracker.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { SearchBar } from '../../components/header/search-bar.component';
 import { Calendar } from '../../components/calendar/calendar';
 import {DatePipe} from '@angular/common';
+import {TrackerCard} from './tracker-card/tracker-card';
 
 @Component({
   selector: 'app-tracker',
@@ -9,22 +10,37 @@ import {DatePipe} from '@angular/common';
     SearchBar,
     Calendar,
     DatePipe,
-    Calendar
+    Calendar,
+    TrackerCard
   ],
   template: `
+    <div class="sticky top-0 z-10 bg-base-100">
     <app-search-bar placeHolderName="Search in tracker"/>
-
     <app-calendar
       [selectedDate]="selected()"
       [highlightedDates]="specialDates()"
       (dateSelected)="onDateSelected($event)">
     </app-calendar>
-
-    @if (selected()){
-    <div  class="mt-4 p-4 text-center">
-      Selected Date: {{ selected() | date: 'fullDate' }}
+      <h1 class="h-10 text-base-content/90">Activities</h1>
     </div>
-    }
+    <section class="mt-4 mb-28 flex flex-col gap-4">
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+      <app-tracker-card/>
+    </section>
+
   `,
   styles: ``
 })
