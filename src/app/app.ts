06947554ce5a102c9ff452a668e78b0f88546d6f
amrohan <rohanxsalunkhe@gmail.com>
2026-03-19 11:38:22 +0530

    feat: add view transistion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Dock } from './components/dock/dock';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {Dock} from './components/dock/dock';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +9,5 @@ import { Dock } from './components/dock/dock';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('orivio');
+
 }
